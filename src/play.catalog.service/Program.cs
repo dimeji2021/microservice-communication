a99f928c9f5ce548f7ac3ae85d134c9c8a489f82
Kehinde Aregbesola <adedimeji88@yahoo.com>
2023-01-19 00:52:30 +0100

    commit

--- src/play.catalog.service/Program.cs
+++ src/play.catalog.service/Program.cs
@@ -4,7 +4,9 @@ using play.common.MongoDB;
 // using Microsoft.Extensions.DependencyInjection;
 using play.common.MassTransit;
 
+
 var builder = WebApplication.CreateBuilder(args);
+var configValue = builder.Configuration.GetSection("AllowedOrigin").Get<string[]>();
 // Add services to the container.
 
 builder.Services.AddMongo()
@@ -25,6 +27,13 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    app.UseCors(builder =>
+    {
+        builder.WithOrigins(configValue) // To allow the origin specified in this appsettings.development, can either take in a string or an array of strings
+        .AllowAnyHeader()
+        .AllowAnyMethod();
+    });
+    // app.UseCors(x => x.AllowAnyHeader().AllowAnyMethod().AllowAnyOrigin()); // To allow any origin
 }
 
 app.UseHttpsRedirection();
